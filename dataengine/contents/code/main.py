006353345540bfde412b46ed68320fd68d5351e0
Thomas Coopman <thomas.coopman@gmail.com>
2009-01-19 21:54:13 +0100

    added initial dataengine

---
+++ dataengine/contents/code/main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+
+from PyQt4.QtCore import *
+from PyKDE4.kdecore import *
+from PyKDE4 import plasmascript
+
+class PyKotnetEngine(plasmascript.DataEngine):
+    def __init__(self,parent,args=None):
+        plasmascript.DataEngine.__init__(self,parent)
+
+    def init(self):
+        #update mostly once in a minutes = 1000msec * 60
+        self.setMinimumPollingInterval(1000 * 60)
+        #update default once in 10 minutes
+        self.setPollingInterval(1000 * 60 * 10)
+
+    def sources(self):
+        sources = ["Local"]
+        sources.extend(KSystemTimeZones.zones().keys())
+        return sources
+
+    # request a new DataEngine
+    def sourceRequestEvent(self, name):
+        #create an empty source
+        self.setData(name, self.Data());
+    
+        return self.updateSourceEvent(name);
+
+    def updateSourceEvent(self, name):
+        #TODO implement
+        return True
+
+def CreateDataEngine(parent):
+    return PyKotnetEngine(parent)
