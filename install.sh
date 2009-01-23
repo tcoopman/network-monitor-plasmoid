6ae7e4f6966c0b336c6df3381d22bf3821a083cd
Thomas Coopman <thomas.coopman@gmail.com>
2009-01-23 16:46:25 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd dataengine
-zip -r ../kotnetlogindataengine.zip
+zip -r ../kotnetlogindataengine.zip .
 cd ..
 plasmapkg -i kotnetlogindataengine.zip
 
 cd applet
-zip -r ../kotnetloginplasmoid.zip
+zip -r ../kotnetloginplasmoid.zip .
 cd ..
 plasmapkg -i kotnetloginplasmoid.zip
--- remove.sh
+++ remove.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 plasmapkg -r kotnet-dataengine
+rm kotnetlogindataengine.zip
 
 plasmapkg -r kotnet-plasmoid
+rm kotnetloginplasmoid.zip
