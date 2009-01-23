571878ddad613a564a7564c913ed0b98a41a3082
Thomas Coopman <thomas.coopman@gmail.com>
2009-01-23 17:31:28 +0100

    begin of working dataengine

--- dataengine/contents/code/main.py
+++ dataengine/contents/code/main.py
@@ -5,11 +5,13 @@ from PyQt4.QtCore import *
 from PyKDE4.kdecore import *
 from PyKDE4 import plasmascript
 
-import kotnetlogin
+from kotnetlogin import KotnetReader
 
 class PyKotnetEngine(plasmascript.DataEngine):
     def __init__(self,parent,args=None):
         plasmascript.DataEngine.__init__(self,parent)
+        self.username = "USER"
+        self.password = "PASS"
 
     def init(self):
         #update mostly once in a minutes = 1000msec * 60
@@ -18,22 +20,26 @@ class PyKotnetEngine(plasmascript.DataEngine):
         self.setPollingInterval(1000 * 60 * 10)
 
     def sources(self):
-        sources = ["Local"]
-        sources.extend(KSystemTimeZones.zones().keys())
-        return sources
+        self.sources = []
+        return self.sources
+        #sources.extend(KSystemTimeZones.zones().keys())
+        #return sources
+        #pass
 
     # request a new DataEngine
     def sourceRequestEvent(self, name):
         #create an empty source
-        self.setData(name, self.Data())
+        print(name)
+        self.sources.append(name)
         self.reader = KotnetReader()
     
         return self.updateSourceEvent(name)
 
     def updateSourceEvent(self, name):
         #TODO implement
-        #info = self.reader.login(username, password)
+        info = self.reader.login(self.username, self.password)
         #set data to info
+        self.setData(name, "downloadAvailable", QVariant(info.download.available))
         return True
     
 
